77e402011efd722d873761f4f9f6cacbd637ae1f
Grey <greydesjardins46@gmail.com>
2026-04-24 10:39:44 -0400

    modified fog color

--- Assets/Scripts/FogManager.cs
+++ Assets/Scripts/FogManager.cs
@@ -5,6 +5,6 @@ public class FogFade : MonoBehaviour
     void Start()
     {
         RenderSettings.fog = true;
-        RenderSettings.fogColor = Color.gray7;
+        RenderSettings.fogColor = new Color(0.6784314f,0.6941177f,0.7058824f,1f);
     }
 }
